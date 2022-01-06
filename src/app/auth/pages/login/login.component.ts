ea2b23df61b1510f987b2504e31f0113e8226b0e
Edwin Granados <edwingranados17@hotmail.com>
2022-01-05 23:49:59 -0500

    heroes app terminado

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router:Router,
+    private authService:AuthService
+  ) { }
+
+  login(){
+
+    this.authService.login().subscribe(usuario => {
+      if(usuario.id){
+        this.router.navigate(['./heroes']);
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  get auth():Auth{
+    return {...this.authService.auth}
+  }
+
+  constructor(
+    private router:Router,
+    private authService :AuthService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  logout(){
+    this.router.navigate(['./auth']);
+  }
 }
